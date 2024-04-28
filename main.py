ed220fcc88fe64cd55cf876a79cde4061e8cdbdf
shioudidi <alanchu0108@g.ncu.edu.tw>
2024-04-28 23:15:59 +0800

    change main and user.py

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-from router import items, whiteboard
+from router.users import add_middleware
+
+from router import items, whiteboard, users
 
 app = FastAPI()
 app.include_router(items.router)
 app.include_router(whiteboard.router)
+app.include_router(users.router)
+
+# add middleware
+add_middleware(app)
 
 app.mount("/static", StaticFiles(directory="static"))
 
--- router/users.py
+++ router/users.py
@@ -1,4 +1,75 @@
 from fastapi import APIRouter
+from starlette.requests import Request
+from starlette.responses import RedirectResponse
+from starlette.middleware.sessions import SessionMiddleware
+from authlib.integrations.starlette_client import OAuth, OAuthError
+from fastapi.templating import Jinja2Templates
 
-router = APIRouter()
+router = APIRouter(
+    prefix="/google",
+    tags=["google"],
+    )
 
+# cuz middleware add to APIRrouter instance "won't" have effect
+# so we need to add it to FastAPI instance(app)
+def add_middleware(app):
+    app.add_middleware(SessionMiddleware, secret_key="add string(any)")
+
+templates = Jinja2Templates(directory="templates")
+
+# 脩的憑證
+GOOGLE_CLIENT_ID = 
+GOOGLE_CLIENT_SECRET = 
+GOOGLE_REDIRECT_URI = 
+
+
+oauth = OAuth()
+oauth.register(
+    name="google",
+    sever_metadata_url="https://accounts.google.com/.well-known/openid-configuration",
+    client_id=GOOGLE_CLIENT_ID,
+    client_secret=GOOGLE_CLIENT_SECRET,
+    redirect_uri=GOOGLE_REDIRECT_URI,
+    client_kwargs={"scope": "openid profile email"},
+)
+
+@router.get("/")
+def index(request: Request):
+    user = request.session.get('user')
+    if user:
+        return RedirectResponse('[to be chosen]')
+
+    return templates.TemplateResponse(
+        name="[to be chosen].html",
+        context={"request": request}
+    )
+
+
+# login
+@router.get("/login")
+async def login(request: Request):
+    url = request.url_for('auth')
+    return await oauth.google.authorize_redirect(request, url)
+    
+    
+@router.get("/auth")
+async def auth_google(request: Request):
+    try:
+        token = await oauth.google.authorize_access_token(request)
+    except OAuthError as e:
+        return templates.TemplateResponse(
+            name='[to be chosen].html',
+            context={'request': request, 'error': e.error}
+        )
+    user = token.get('userinfo')
+    if user:
+        request.session['user'] = dict(user)
+    return RedirectResponse('[to be chosen]')
+
+# logout
+@router.get('/logout')
+def logout(request: Request):
+    request.session.pop('user')
+    request.session.clear()
+    return RedirectResponse('/')
+    
