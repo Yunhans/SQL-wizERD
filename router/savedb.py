58adb3555f0d9910f0e57e31218c3da4b8e95727
HenryLin <henry04.lin@gmail.com>
2024-05-22 14:15:43 +0800

    add savedb router

---
+++ router/savedb.py
@@ -0,0 +1,43 @@
+import mysql.connector
+
+
+'''
+def new_user(name, mail):
+    connection = connect_to_database()
+    if connection.is_connected():
+        cursor = connection.cursor()
+        sql_insert_query = """ INSERT INTO `user` (`Name`, `Mail`) VALUES (%s, %s) """
+        cursor.execute(sql_insert_query, (name, mail))
+        connection.commit()
+        print("資料已成功插入")
+'''
+
+#新增使用者
+def new_user():
+    connection = connect_to_database()
+    if connection.is_connected():
+        cursor = connection.cursor()
+        sql_insert_query = """ INSERT INTO `user` (`Name`, `Mail`) VALUES ('John', 'john@example.com') """
+        cursor.execute(sql_insert_query)
+        connection.commit()
+        print("Successfully inserted")
+
+
+
+#連接到資料庫
+def connect_to_database():
+    connection = mysql.connector.connect(
+        host="localhost",  # 通常為localhost
+        user="admin",  # 輸入您的phpMyAdmin的使用者名稱
+        password="123",  # 輸入您的phpMyAdmin的密碼
+        database="graduate_project"  # 輸入您想要連接的資料庫名稱
+    )
+
+    if connection.is_connected():
+        print("Successfully connected")
+    else:
+        print("Failed")
+    return connection
+
+
+
