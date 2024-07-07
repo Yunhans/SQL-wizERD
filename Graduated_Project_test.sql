6e295cc7e481bd7eee3bbc13bfaa8284081fe8ee
shioudidi <alanchu0108@g.ncu.edu.tw>
2024-07-07 22:41:04 +0800

    modify sql schema

--- Graduated_Project_test.sql
+++ Graduated_Project_test.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE  IF NOT EXISTS `grduated_project` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `grduated_project`;
+CREATE DATABASE  IF NOT EXISTS `graduated_project` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `graduated_project`;
 -- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: grduated_project
