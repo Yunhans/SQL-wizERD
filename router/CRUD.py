9870bfad1abbf28c5dee5dfdbc0704cc10a9b960
HenryLin <henry04.lin@gmail.com>
2024-07-09 00:13:37 +0800

    add insert function

--- router/CRUD.py
+++ router/CRUD.py
@@ -4,6 +4,54 @@ from connect_db import connect_to_database, close_connection
 
 
 
+
+#-------------------------新增功能---------------------------------#
+
+#建立新table
+def new_table (Table_name, script, file_id):
+    connection = connect_to_database()
+    if connection.is_connected():
+        cursor = connection.cursor()
+        sql_insert_query = """ INSERT INTO `table_list` (`x`, `y`,`Table_name`,`script`,`File`) VALUES (0, 0, %s, %s,%s)"""
+        cursor.execute(sql_insert_query, (Table_name, script, file_id))
+        connection.commit()
+        print("successfully added new table!")
+
+#建立新使用者
+def new_user(mail):
+    connection = connect_to_database()
+    if connection.is_connected():
+        cursor = connection.cursor()
+        sql_insert_query = """ INSERT INTO `users` ( `Mail`) VALUES (%s) """
+        cursor.execute(sql_insert_query, (mail,))
+        connection.commit()
+        print("Successfully added new user!")
+
+#建立新檔案
+def new_file(file_name, user_id):
+    connection = connect_to_database()
+    if connection.is_connected():
+        cursor = connection.cursor()
+        sql_insert_query = """ INSERT INTO `files` ( `File_name`,`User_id`) VALUES (%s,%s) """
+        cursor.execute(sql_insert_query, (file_name, user_id))
+        connection.commit()
+        print("Successfully added new file!")
+
+#建立新外鍵
+def new_foreign_key(from_table, ref_table, from_column, ref_column, table_id):
+    connection = connect_to_database()
+    if connection.is_connected():
+        cursor = connection.cursor()
+        sql_insert_query = """ INSERT INTO `foreign_key` ( `From_tbl`,`Ref_tbl`,`From_col`, `To_col`, `table_id`) VALUES (%s,%s,%s,%s,%s) """
+        cursor.execute(sql_insert_query, (from_table, ref_table, from_column, ref_column, table_id))
+        connection.commit()
+        print("Successfully added new foreign key!")
+
+new_user("123777@gmail.com")
+
+
+#-------------------------修改功能---------------------------------#
+
 '''
 更新table
 def update_table(x, y, script,t_id):
@@ -17,51 +65,7 @@ def update_table(x, y, script,t_id):
 '''
 
 
-#建立新table
-def new_table (x, y, script,file_id):
-    connection = connect_to_database()
-    if connection.is_connected():
-        cursor = connection.cursor()
-        sql_insert_query = """ INSERT INTO `table_list` (`x`, `y`,`script`,`File`) VALUES (%s, %s, %s,%s)"""
-        cursor.execute(sql_insert_query, (0,0,1, 1))
-        connection.commit()
-        print("successfully added new table!")
-
-new_table(0,0,1,1)
-
-'''
-更新script
-def update_script(script, s_id):
-    connection = connect_to_database()
-    if connection.is_connected():
-        cursor = connection.cursor()
-        sql_insert_query = """ UPDATE `Script` SET `script` = %s WHERE `s_id` = %s"""
-        cursor.execute(sql_update_query, (script, s_id))
-        connection.commit()
-        print("Successfully updated script!")
-'''
-
-
-
-
-'''
-建立新使用者
-def new_user(name, mail):
-    connection = connect_to_database()
-    if connection.is_connected():
-        cursor = connection.cursor()
-        sql_insert_query = """ INSERT INTO `user` (`Name`, `Mail`) VALUES (%s, %s) """
-        cursor.execute(sql_insert_query, (name, mail))
-        connection.commit()
-        print("Successfully added new user!")
-'''
-
-
-
-
-
-
-
+#-------------------------刪除功能---------------------------------#
 
 
 
