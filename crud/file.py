47c5b32abb750dd85669f5081a9306b52632032a
shioudidi <alanchu0108@g.ncu.edu.tw>
2024-07-15 21:12:01 +0800

    add create new file api

--- crud/file.py
+++ crud/file.py
@@ -9,15 +9,22 @@ from connect_db import connect_to_database, close_connection
 '''
 
 def new_file(file_name, user_id):
-    connection = connect_to_database()
-    if connection.is_connected():
-        cursor = connection.cursor()
-        sql_insert_query = """ INSERT INTO `tbl_file` ( `file_name`,`user_id`) VALUES (%s,%s) """
-        cursor.execute(sql_insert_query, (file_name, user_id))
-        connection.commit()
-        print("Successfully added new file!")
+    try:
+        connection = connect_to_database()
+        if connection.is_connected():
+            cursor = connection.cursor()
+            sql_insert_query = """ INSERT INTO `tbl_file` (`file_name`, `user_id`) VALUES (%s, %s) """
+            cursor.execute(sql_insert_query, (file_name, user_id))
+            connection.commit()
+            return "successfully add new file"
         
-        
+    except Exception as e:
+        return f"failed to add new file: {e}"
+    
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
         
         
 '''
@@ -27,11 +34,21 @@ def new_file(file_name, user_id):
 '''
 
 def get_all_files(user_id):
-    connection = connect_to_database()
-    if connection.is_connected():
-        cursor = connection.cursor()
-        sql_search_query = """ SELECT `file_id`,`file_name` FROM `tbl_file` WHERE `user_id` = %s"""
-        cursor.execute(sql_search_query, (user_id,))
-        records = cursor.fetchall()
-        return records
-    # example records: [(1, 'file1'), (2, 'file2'), (3, 'file3')]
+    try:
+        connection = connect_to_database()
+        if connection.is_connected():
+            cursor = connection.cursor()
+            sql_search_query = """ SELECT `file_id`, `file_name` FROM `tbl_file` WHERE `user_id` = %s"""
+            cursor.execute(sql_search_query, (user_id,))
+            records = cursor.fetchall()
+            return records
+            # example records: [(1, 'file1'), (2, 'file2'), (3, 'file3')]
+        
+    except Exception as e:
+        return f"Failed to retrieve files: {e}"
+    
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+    
--- router/files.py
+++ router/files.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
 
-from crud.file import get_all_files
+from crud.file import new_file, get_all_files
 
 
 
@@ -12,9 +12,25 @@ router = APIRouter(
 
 
 '''
+--- CREATE ---
 
---- get all the files ---
+    -create new file
+    -params: user_id, file_name
 
+'''
+
+@router.post("/create/{user_id}/{file_name}")
+async def create_new_file(user_id, file_name):
+    message = new_file(file_name, user_id)
+    
+    return {"message": message}
+
+
+
+'''
+--- READ ---
+
+    -get all the files
     -params: user_id 
 
 '''
@@ -22,4 +38,5 @@ router = APIRouter(
 @router.get("/get/{user_id}")
 async def get_all_file(user_id):
     file_list = get_all_files(user_id)
+    
     return {"files": file_list}
