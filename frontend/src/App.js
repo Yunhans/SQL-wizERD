90b516c7dfddabeb123c5594242f589fc9c454e1
Yunhans <hugo52323@yahoo.com>
2024-08-16 01:12:53 +0800

    get specific table data in edit table form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,7 +20,7 @@ const edgeTypes = {
 const initialNodes = [
 	{
 		id: '1',
-		"type": 'table',
+		type: 'table',
 		data: {
 			id: '1',
 			label: 'Product',
@@ -70,7 +70,8 @@ function App() {
 		const newNode = {
 		  id: getNodeId(),
 		  type: 'table',
-		  data: { 
+		  data: {
+			id: 'id',
 			label: 'new Table',
 			attribute:[
 				{ name: 'id', type: 'INT', isKey: true }
@@ -89,17 +90,85 @@ function App() {
 	
 		function receiveMessage(e) {
 			// console.log('Message received:', e);
-			try {
+			try {				
 				const data = JSON.parse(e.data);
-				console.log('Data received:', data);
-	
-				// Process the received data and update nodes or edges if needed
-				try {
-					setNodes(data.nodes);
-                    setEdges(data.edges);
-				} catch (error) {
-					console.error('Failed to parse or apply data:', error);
-				}
+				// console.log('Iframe Data received:', data);
+
+				if (data.task === 'allTables') {
+					// Process the received data and update nodes or edges if needed
+					try {
+						setNodes(data.nodes);
+						setEdges(data.edges);
+					} catch (error) {
+						console.error('Failed to parse or apply data:', error);
+					}
+
+				} 
+				else if (data.task === 'specificTable') {
+					console.log('Iframe Data received:', data.table_data);
+					const table_data = data.table_data;
+					document.getElementById('inputTableName').value = table_data.name;
+
+					var attribute_html = '';
+					table_data.attribute.forEach(function(value, index){
+						attribute_html += '\
+							<div class="row mb-2">\
+								<div class="col-md-4">\
+									<input type="text" class="form-control" id="inputAttrName-'+ index + '" value='+ value.name +'>\
+								</div>\
+								<div class="col-md-4">\
+									<input class="form-control" list="datalistOptions" id="inputType-'+ index + '" value='+ value.type +'>\
+									<datalist id="datalistOptions">\
+										<option value="INT">\
+										<option value="VARCHAR(255)">\
+										<option value="DATE">\
+									</datalist>\
+								</div>\
+								<div class="col-md-4 d-flex justify-content-around">\
+									<!-- primary key -->\
+									<input type="checkbox" class="btn-check" id="key-check-'+ index + '" autocomplete="off" '+ (value.primary_key? "checked" : "") +' >\
+									<label class="btn btn-outline-warning" for="key-check-'+ index + '" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="primary key">\
+										<i class="bi bi-key-fill"></i>\
+									</label>\
+									<!-- notnull -->\
+									<input type="checkbox" class="btn-check" id="null-check-'+ index + '" autocomplete="off" '+ (value.not_null? "checked" : "") +'>\
+									<label class="btn btn-outline-danger" for="null-check-'+ index + '" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="not null">\
+										<i class="bi bi-ban"></i>\
+									</label>\
+									<!-- unique -->\
+									<input type="checkbox" class="btn-check" id="uni-check-'+ index + '" autocomplete="off" '+ (value.unique? "checked" : "") +'>\
+									<label class="btn btn-outline-dark" for="uni-check-'+ index + '" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="unique">\
+										<i class="bi bi-star-fill"></i>\
+									</label>	\
+									<!-- edit -->\
+									<div class="btn-group">\
+										<button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">\
+											<i class="bi bi-three-dots"></i>\
+										</button>\
+										<div class="dropdown-menu" style="min-width: 250px;">\
+											<p class="dropdown-header">Other Constraints</p>\
+											<div class="form-check mx-3 mb-2">\
+												<input class="form-check-input" type="checkbox" value="" id="auto-check-'+ index + '"  >\
+												<label class="form-check-label" for="auto-check-'+ index + '">\
+													auto increment\
+												</label>\
+											</div>\
+											<div class="mx-3">\
+												<label for="default-'+ index + '" class="form-label mb-0">default</label>\
+												<input type="text" class="form-control form-control-sm" id="default-'+ index + '">\
+											</div>\
+											<div class="dropdown-divider"></div>\
+											<p class="dropdown-header">Actions</p>\
+											<button type="button" class="btn btn-sm btn-danger mx-3 mb-2"><i class="bi bi-trash3"></i> Delete attribute</button>\
+										</div>\
+									</div>\
+								</div>\
+							</div>\
+						';	
+					});
+
+					document.getElementById('attribute-container').innerHTML = attribute_html;
+				}				
 
 			} catch (error) {
 				console.error('Failed to parse data:', e.data);
--- frontend/src/TableNode.js
+++ frontend/src/TableNode.js
@@ -7,12 +7,12 @@ const TableNode = ({ data }) => {
             <div className="mytable card" draggable="true" style={{ width: '300px' }}>
                 <div className="myheader card-header d-flex px-2" draggable="true">
                     <span className="flex-grow-1 text-center">{data.label}</span>
-                    <button type="button" className="btn btn-sm border-0 p-0" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                    <button type="button" className="btn btn-sm border-0 p-0" onClick={ () => { const message = JSON.stringify({ action: 'edit info', table_id: data.id }); window.parent.postMessage(message, 'http://127.0.0.1:8000/whiteboard/'); } } data-bs-toggle="modal" data-bs-target="#editTableModal">
                         <i className="bi bi-three-dots-vertical"></i>
                     </button>
                 </div>
                 <ul className="list-group list-group-flush my-1">
-                    {data.attribute.map((attribute, index) => (
+                    { data.attribute.map((attribute, index) => (
                         <li key={index} className="list-group-item border-0 py-1">
                             <div className="my-0 w-100 d-flex justify-content-between">
                                 <span className="text-secondary">{attribute.name} {attribute.isKey && <i className="bi bi-key-fill text-warning"></i>}</span>
